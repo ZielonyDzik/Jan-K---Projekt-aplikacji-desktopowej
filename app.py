55c461b37461eddd1df06b1114043730573dbd7b
ZielonyDzik <janekkapuscinski05@gmail.com>
2022-11-16 00:46:27 +0100

    Add project files - Zielony Las

---
+++ app.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("500x500")
+root.title('Zielony Las')
+root.configure(bg='#395814')
+root.iconbitmap('zielonydzik.ico')
+
+#==================================#
+#==================================#
+#==================================#
+
+root.mainloop()
